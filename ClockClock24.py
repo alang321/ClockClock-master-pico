ede1367adc0a60eed10afc83186d9d574522e959
andonius <34992832+alang321@users.noreply.github.com>
2021-12-13 12:29:46 +0100

    Sleep mode

--- ClockClock24.py
+++ ClockClock24.py
@@ -1,4 +1,5 @@
 import machine
+import time
 from ClockStepper import ClockStepper
 from DigitDisplay import DigitDisplay
 from ClockModule import ClockModule
@@ -8,6 +9,7 @@ class ClockClock24:
     
     def __init__(self, slave_adr_list: List[int], i2c_bus_list: List[machine.I2C], steps_full_rev = 4320):
         self.steps_full_rev = steps_full_rev
+        self.is_asleep = False
         
         self.clock_modules = [ClockModule(i2c_bus_list[module_index], slave_adr_list[module_index]) for module_index in range(len(slave_adr_list))]
         
@@ -19,14 +21,16 @@ class ClockClock24:
     def display_digit(self, index: int, number: int, direction = 0, extra_revs = 0):
         self.digit_displays[index].display(number, direction, extra_revs)
         
-    def display_digit(self, index: int, number: int, direction = 0, extra_revs = 0):
-        self.digit_displays[index].display(number, direction, extra_revs)
-        
     def enable_disable_driver(self, enable_disable: bool):
         """
         true to enable driver of module
         false to disable
         """
+        if enable_disable:
+            self.is_asleep = False
+        else:
+            self.is_asleep = True
+            
         for module in self.clock_modules:
             module.enable_disable_driver_module(enable_disable)
     
@@ -38,25 +42,30 @@ class ClockClock24:
         for module in self.clock_modules:
             module.set_accel_module(accel)
     
-    def move_to_all(self, position: int, direction: int):
-        for module in self.clock_modules:
-            module.move_to_module(position, direction)
+    def move_to_all(self, position: int, direction = 0):
+        if(!self.is_asleep):
+            for module in self.clock_modules:
+                module.move_to_module(position, direction)
     
     def move_to_extra_revs_all(self, position: int, direction: int, extra_revs: int):
-        for module in self.clock_modules:
-            module.move_to_extra_revs_module(position, direction, extra_revs)
+        if(!self.is_asleep):
+            for module in self.clock_modules:
+                module.move_to_extra_revs_module(position, direction, extra_revs)
     
     def move_all(self, distance: int, direction: int):
-        for module in self.clock_modules:
-            module.move_module(distance, direction)
+        if(!self.is_asleep):
+            for module in self.clock_modules:
+                module.move_module(distance, direction)
         
     def stop_all(self):
-        for module in self.clock_modules:
-            module.stop_module()
+        if(!self.is_asleep):
+            for module in self.clock_modules:
+                module.stop_module()
     
     def falling_pointer(self):
-        for module in self.clock_modules:
-            module.falling_pointer_module()
+        if(!self.is_asleep):
+            for module in self.clock_modules:
+                module.falling_pointer_module()
 
     def is_running(self) -> bool: #returns True if stepper is running
         for module in self.clock_modules:
@@ -64,3 +73,14 @@ class ClockClock24:
                 return True
         
         return False
+    
+    def sleep(self):
+        self.move_to_all(int(0.5*self.steps_full_rev))
+        
+        while self.is_running:
+            time.sleep(0.2)
+            
+        self.enable_disable_driver(False)
+    
+    def wake_up(self):
+        self.enable_disable_driver(True)
