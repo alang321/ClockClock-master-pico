ccfcf23f511bcb363ac22e1a1fa94783ce70386c
alang321 <34992832+alang321@users.noreply.github.com>
2022-11-16 12:21:57 +0100

    initial settings page classe proto

---
+++ settings.py
@@ -0,0 +1,30 @@
+
+
+
+
+class SettingsPage:
+    
+  def __init__(self, clockclock, persistent_storage input_locked=True):
+    self.firstname = fname
+    self.lastname = lname
+    
+    self.__input_locked = input_locked
+    
+  def page_constructor(self):
+      
+  def page_destructor(self):
+
+  def button_up(self):
+    print(self.firstname, self.lastname)
+
+  def button_down(self):
+    print(self.firstname, self.lastname)
+
+  def next_digit(self):
+    print(self.firstname, self.lastname)
+    
+  def new_time(self):
+      return
+    
+  def lock_input(self, lock):
+    self.__input_locked = locke
